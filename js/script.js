3bf5e8adb9372fe4faa3492a7eaa6ea450875bb1
abdi-hu <78183395+abdi-hu@users.noreply.github.com>
2021-05-15 15:21:52 -0400

    fix issue with scroll reveal

--- js/script.js
+++ js/script.js
@@ -1,25 +1,14 @@
 (function () {
-    const srTop = ScrollReveal({
+    const sr = ScrollReveal({
         origin: 'top',
         distance: '50px',
         duration: 2000,
         reset: true
     });
-    srTop.reveal('h1', {delay: 300})
-    srTop.reveal('.project-image', {delay: 300})
-    const srBottom = ScrollReveal({
-        origin: 'bottom',
-        distance: '80px',
-        duration: 2000,
-        reset: true
-    });
-    srBottom.reveal('.skill-icons>img', { delay: 300 })
-    const srRight= ScrollReveal({
-        origin: 'right',
-        distance: '80px',
-        duration: 2000,
-        reset: true
-    });    
-    srRight.reveal('.contact-icons', {delay: 300})
+     
+    sr.reveal('.skill-icons>img', { delay: 200 })
+    sr.reveal('h1', {delay: 200})
+    sr.reveal('.project-image', {delay: 200})
+    sr.reveal('.contact-icons', {delay: 200})
 })();
 
