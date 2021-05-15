01156e3e35aaa5513beeb9a434ca540a75cdc9bd
abdi-hu <78183395+abdi-hu@users.noreply.github.com>
2021-05-15 15:17:28 -0400

    added contact and styling

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-});
+(function () {
+    const srTop = ScrollReveal({
+        origin: 'top',
+        distance: '50px',
+        duration: 2000,
+        reset: true
+    });
+    srTop.reveal('h1', {delay: 300})
+    srTop.reveal('.project-image', {delay: 300})
+    const srBottom = ScrollReveal({
+        origin: 'bottom',
+        distance: '80px',
+        duration: 2000,
+        reset: true
+    });
+    srBottom.reveal('.skill-icons>img', { delay: 300 })
+    const srRight= ScrollReveal({
+        origin: 'right',
+        distance: '80px',
+        duration: 2000,
+        reset: true
+    });    
+    srRight.reveal('.contact-icons', {delay: 300})
+})();
 
-sr.reveal('.project-image', {delay: 300})
-sr.reveal('.skill-icons>img', {delay: 300})
-// sr.reveal('.project', {delay: 600})
