f4efae25944688bf2a620fbed412c093b679fd18
abdi-hu <78183395+abdi-hu@users.noreply.github.com>
2021-05-15 14:35:55 -0400

    added projects and scrollview

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal('.project-image', {delay: 300})
+sr.reveal('.skill-icons>img', {delay: 300})
+// sr.reveal('.project', {delay: 600})
